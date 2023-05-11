3765397deca7baff17bd26a7ba37fb724196b591
shamar marshall <shamarmarshall23@gmail.com>
2023-05-11 15:03:14 -0400

    added firebase

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,98 +1,103 @@
-import 'package:bimlinkz_mobile_app/widgets/bottom_navbar.dart';
 import 'package:bimlinkz_mobile_app/widgets/category_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  CollectionReference firestore =
+      FirebaseFirestore.instance.collection('Categories');
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(
-            "Bimlinkz",
-            style: GoogleFonts.inter(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Bimlinkz",
+          style: GoogleFonts.inter(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        body: SingleChildScrollView(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 14,
+      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 14,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade300,
+                      blurRadius: 10,
+                      spreadRadius: 5)
+                ],
+                color: const Color(0xFFFFFFFF),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.shade300,
-                        blurRadius: 10,
-                        spreadRadius: 5)
-                  ],
-                  color: Color(0xFFFFFFFF),
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Search...',
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: () {},
+                    ),
+                    prefixIcon: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.search),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Populart searches',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
                 ),
-                child: TextField(
-                  decoration: InputDecoration(
-                      hintText: 'Search...',
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.clear),
-                        onPressed: () {},
-                      ),
-                      prefixIcon: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.search),
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                ),
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                TextButton(onPressed: () {}, child: const Text('See all'))
+              ],
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Container(
+              height: 130,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
                 children: [
-                  const Text(
-                    'Populart searches',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-                  ),
-                  TextButton(onPressed: () {}, child: const Text('See all'))
+                  CategoryCard(),
+                  CategoryCard(),
+                  CategoryCard(),
+                  CategoryCard(),
                 ],
               ),
-              const SizedBox(
-                height: 14,
-              ),
-              Container(
-                height: 130,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    CategoryCard(),
-                    CategoryCard(),
-                    CategoryCard(),
-                    CategoryCard(),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: 338,
-                height: 316,
-                color: Colors.grey,
-              )
-            ],
-          ),
-        )),
-        bottomNavigationBar: BottomNavBar(),
-      ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: 338,
+              height: 316,
+              color: Colors.grey,
+            )
+          ],
+        ),
+      )),
     );
   }
 }
